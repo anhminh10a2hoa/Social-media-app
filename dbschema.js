d55679f76fa21cd69c6738c98511e0d0dad7d74f
Anh Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-08-05 16:24:14 +0300

    connect to firebase

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandler: "user",
+      body: "this is the scream body",
+      createdAt: "2020-08-05T13:20:57.562Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
