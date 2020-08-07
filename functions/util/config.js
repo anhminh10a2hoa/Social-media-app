8b6e638be6a19312cceb5028bd65163a13d74444
Anh Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-08-07 20:09:36 +0300

    Refactoring & Organising

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCgKJjUFhSDS1PytJifLGmkaEcXST432uo",
+  authDomain: "social-app-7d922.firebaseapp.com",
+  databaseURL: "https://social-app-7d922.firebaseio.com",
+  projectId: "social-app-7d922",
+  storageBucket: "social-app-7d922.appspot.com",
+  messagingSenderId: "170956955423",
+  appId: "1:170956955423:web:177461cc8ca1d4d7f3da2a",
+  measurementId: "G-5Z4QJ5FWVY",
+};
