584529df2cfcc2892912f03d694b0ca52e4a6b63
Anh Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-09-11 12:13:12 +0300

    enable cors

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,8 @@ const functions = require("firebase-functions");
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 const { db } = require("./util/admin");
 
 const FBAuth = require("./util/fbAuth");
